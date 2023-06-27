28b0b2b3ebe31abbd106915221e2bd97ae24688b
Juan Felipe Jimenez Lozada <jimenezlozadajuanfelipe@gmail.com>
2023-06-27 16:20:38 -0500

    Hago la conexión a la base de datos (Porque estaba visible para todos)

--- Online-Course-and-Exam-Management-System/Models/PostgresContext.cs
+++ Online-Course-and-Exam-Management-System/Models/PostgresContext.cs
@@ -37,10 +37,7 @@ public partial class PostgresContext : DbContext
 
     public virtual DbSet<Transaccion> Transaccions { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=postgres;Username=postgres;Password=12345;");
-
+    
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.HasPostgresExtension("pg_catalog", "adminpack");
--- Online-Course-and-Exam-Management-System/Program.cs
+++ Online-Course-and-Exam-Management-System/Program.cs
@@ -1,6 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using Online_Course_and_Exam_Management_System.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+//Conexion base de datos
+builder.Services.AddEntityFrameworkNpgsql()
+           .AddDbContext<PostgresContext>(options =>
+               options.UseNpgsql(builder.Configuration.GetConnectionString("CursoExamenConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
