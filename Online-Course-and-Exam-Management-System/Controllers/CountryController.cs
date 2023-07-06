6cdc4440acf4fa6f27d0b76516e4bf5d16f59b74
Juan Felipe Jimenez Lozada <jimenezlozadajuanfelipe@gmail.com>
2023-07-06 18:20:30 -0500

    Agregue una funcion para mostrar todos los datos de la tabla pais junto con un logger(registro de eventos)

--- Online-Course-and-Exam-Management-System/Controllers/CountryController.cs
+++ Online-Course-and-Exam-Management-System/Controllers/CountryController.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using Online_Course_and_Exam_Management_System.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Online_Course_and_Exam_Management_System.Controllers
 {
@@ -9,26 +13,33 @@ namespace Online_Course_and_Exam_Management_System.Controllers
     public class CountriesController : ControllerBase
     {
         private readonly PostgresContext _context;
+        private readonly ILogger<CountriesController> _logger;
 
-        public CountriesController(PostgresContext context)
+        public CountriesController(PostgresContext context, ILogger<CountriesController> logger)
         {
             _context = context;
+            _logger = logger;
         }
 
-        [HttpGet("paises")]
+        [HttpGet]
         public async Task<ActionResult<IEnumerable<Pai>>> GetPaisesFromStoredProcedure()
         {
             try
             {
-                var paises = await _context.Pais.FromSqlRaw("CALL public.getpaises()").ToListAsync();
+                _logger.LogInformation("Ejecutando consulta del procedimiento almacenado");
+
+                var paises = await _context.Pais.FromSqlRaw("SELECT * FROM mostrar_datos();").ToListAsync();
+
+                _logger.LogInformation("Consulta ejecutada exitosamente");
+
                 return Ok(paises);
             }
             catch (Exception ex)
             {
+                _logger.LogError(ex, "Error al obtener los países desde el procedimiento almacenado");
+
                 return StatusCode(500, $"Error al obtener los países desde el procedimiento almacenado: {ex.Message}");
             }
         }
-
-    
     }
 }
