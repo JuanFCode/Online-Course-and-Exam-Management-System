d0a0025bd0adec2f7e016c7cb3a5755568df3d70
Juan Felipe Jimenez Lozada <jimenezlozadajuanfelipe@gmail.com>
2023-07-05 22:46:16 -0500

    Mejorando la legibilidad del código

--- Online-Course-and-Exam-Management-System/Controllers/CountryController.cs
+++ Online-Course-and-Exam-Management-System/Controllers/CountryController.cs
@@ -5,17 +5,16 @@ namespace Online_Course_and_Exam_Management_System.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ModelController : ControllerBase
+    public class CountriesController : ControllerBase
     {
         private readonly PostgresContext _context;
 
-        public ModelController(PostgresContext context)
+        public CountriesController(PostgresContext context)
         {
             _context = context;
         }
 
-        // metodo get() para mostrar todos los datos de la tabla pais
-        [HttpGet("procedimiento")]
+        [HttpGet("paises")]
         public async Task<ActionResult<IEnumerable<Pai>>> GetPaisesFromStoredProcedure()
         {
             try
@@ -25,12 +24,8 @@ namespace Online_Course_and_Exam_Management_System.Controllers
             }
             catch (Exception ex)
             {
-                return StatusCode(500, "Ocurrió un error al obtener los países desde el procedimiento almacenado: " + ex.Message);
+                return StatusCode(500, $"Error al obtener los países desde el procedimiento almacenado: {ex.Message}");
             }
         }
-
-
     }
 }
-
-
