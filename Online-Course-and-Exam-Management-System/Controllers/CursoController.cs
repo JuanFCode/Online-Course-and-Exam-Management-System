920951504146d94557d913835d99976de6ed7250
Juan Felipe Jimenez Lozada <jimenezlozadajuanfelipe@gmail.com>
2023-07-10 20:50:07 -0500

    Cree controlador curso

---
+++ Online-Course-and-Exam-Management-System/Controllers/CursoController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Online_Course_and_Exam_Management_System.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CursoController : ControllerBase
+    {
+    }
+}
