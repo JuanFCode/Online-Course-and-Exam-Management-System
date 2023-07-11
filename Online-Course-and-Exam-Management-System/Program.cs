7f69b0d9aa50dc1104d05a45e339659fdd8150f6
Juan Felipe Jimenez Lozada <jimenezlozadajuanfelipe@gmail.com>
2023-07-10 20:45:30 -0500

    Update errores

--- Online-Course-and-Exam-Management-System/Program.cs
+++ Online-Course-and-Exam-Management-System/Program.cs
@@ -1,4 +1,4 @@
-global using Microsoft.EntityFrameworkCore;
+﻿global using Microsoft.EntityFrameworkCore;
 using Online_Course_and_Exam_Management_System.Models;
 
 var builder = WebApplication.CreateBuilder(args);
