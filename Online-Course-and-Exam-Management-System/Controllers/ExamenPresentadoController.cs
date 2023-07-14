7ce4b8e69a051a2266ebe944652ee918c1e84d8e
Juan Felipe Jimenez Lozada <jimenezlozadajuanfelipe@gmail.com>
2023-07-14 15:08:32 -0500

    API REST table( examenpresentado)

--- Online-Course-and-Exam-Management-System/Controllers/ExamenPresentadoController.cs
+++ Online-Course-and-Exam-Management-System/Controllers/ExamenPresentadoController.cs
@@ -90,6 +90,94 @@ namespace Online_Course_and_Exam_Management_System.Controllers
             }
         }
 
+        // PUT: api/ExamenPresentado
+        [HttpPut("Putpregunta/{id}")]
+        public async Task<IActionResult> PutExamenPresentado(int id, [FromBody] Examenpresentado examenpresentado)
+        {
+            try
+            {
+                _logger.LogInformation($"Actualizando ExamenPresentado con ID : {id}");
+
+                // Verificar si el ID en el cuerpo de la solicitud coincide con el ID proporcionado en la ruta
+                if (id != examenpresentado.Id)
+                {
+                    return BadRequest("El ID de la transaccion en el cuerpo de la solicitud no coincide con el ID proporcionado en la ruta.");
+                }
+
+                // Buscar el curso existente en la base de datos utilizando el ID
+                var ExamenPresentadoExistente = await _context.Examenpresentados.FindAsync(id);
+
+                // Si no se encuentra la transaccion, devolver una respuesta de error NotFound
+                if (ExamenPresentadoExistente == null)
+                {
+                    return NotFound($"No se encontró ExamenPresentado con ID: {id}");
+                }
+
+                ExamenPresentadoExistente.Tercero = examenpresentado.Tercero ?? ExamenPresentadoExistente.Tercero;
+                ExamenPresentadoExistente.Examen = examenpresentado.Examen ?? ExamenPresentadoExistente.Examen;
+                ExamenPresentadoExistente.Fechainicio = examenpresentado.Fechainicio ?? ExamenPresentadoExistente.Fechainicio;
+                ExamenPresentadoExistente.Fechafinal = examenpresentado.Fechafinal ?? ExamenPresentadoExistente.Fechafinal;
+                ExamenPresentadoExistente.Ultimapreguntarespondida = examenpresentado.Ultimapreguntarespondida ?? ExamenPresentadoExistente.Ultimapreguntarespondida;
+                ExamenPresentadoExistente.Estadoexamen = examenpresentado.Estadoexamen ?? ExamenPresentadoExistente.Estadoexamen;
+
+
+
+                // Guardar los cambios en la base de datos
+                await _context.SaveChangesAsync();
+                _logger.LogInformation("ExamenPresentado actualizado exitosamente");
+
+                // Devolver una respuesta sin contenido (NoContent) para indicar que la actualización fue exitosa
+                return NoContent();
+
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error al actualizar ExamenPresentado");
+
+                // Devolver una respuesta de error con un código de estado 500 (Internal Server Error) y un mensaje de error
+                return StatusCode(500, $"Error al actualizar ExamenPresentado: {ex.Message}");
+            }
+        }
+
+        // DELETE: api/ExamenPresentado
+        [HttpDelete("DeleteExamenPresentado/{id}")]
+        public async Task<IActionResult> DeleteExamenPresentado(int id)
+        {
+            try
+            {
+                _logger.LogInformation($"Eliminando ExamenPresentado con ID: {id}");
+
+                var ExamenPresentadoExistente = await _context.Examenpresentados.SingleOrDefaultAsync(c => c.Id == id);
+
+                if (ExamenPresentadoExistente == null)
+                {
+                    return NotFound(); // Devolver una respuesta HTTP 404 (Not Found) si la transaccion no existe
+                }
+
+                _context.Examenpresentados.Remove(ExamenPresentadoExistente);
+                await _context.SaveChangesAsync();
+
+                _logger.LogInformation("ExamenPresentado eliminado exitosamente");
+
+                // Devolver una respuesta HTTP 204 (No Content) para indicar que la eliminación fue exitosa
+                return NoContent();
+            }
+            catch (InvalidOperationException ex)
+            {
+                // Manejar excepciones de operación no válida (InvalidOperation)
+                _logger.LogError(ex, "Error al eliminar ExamenPresentado : Operación no válida");
+                return StatusCode(500, $"Error al eliminar ExamenPresentado: Operación no válida");
+            }
+            catch (Exception ex)
+            {
+                // Manejar excepciones genéricas
+                _logger.LogError(ex, "Error al eliminar la ExamenPresentado");
+
+                // Devolver una respuesta HTTP 500 (Internal Server Error) con un mensaje de error
+                return StatusCode(500, $"Error al eliminar ExamenPresentado : {ex.Message}");
+            }
+        }
+
 
     }
 }
